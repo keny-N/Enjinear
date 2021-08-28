792c79dd3fbabda27815748488b64cd2be21f7cd
ProBowlerAkashi <akashi2468@gmail.com>
2021-08-29 03:50:09 +0900

    WalkAroundにGoogleMapを実装。 経路情報の取得作業中。どんづまっているので応援求む。 9:00起床予定

--- app/src/main/java/com/example/enjinear/WalkAround.java
+++ app/src/main/java/com/example/enjinear/WalkAround.java
@@ -1,32 +1,53 @@
 package com.example.enjinear;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.widget.TextView;
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.widget.TextView;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.example.enjinear.databinding.WalkAroundBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class WalkAround extends AppCompatActivity implements LocationListener {
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-
+public class WalkAround extends AppCompatActivity implements LocationListener, OnMapReadyCallback {
     static final int REQUEST_CAPTURE_IMAGE = 100;
     Button button1;
     ImageView imageView1;
@@ -34,10 +55,24 @@ public class WalkAround extends AppCompatActivity implements LocationListener {
     long startingTime = 0;
     boolean isWalking = false;
 
+    Timer timer;
+
+    private Position currentPosition = new Position();
+    private List<Position> route = new ArrayList<Position>();
+
+    private GoogleMap mMap;
+    private WalkAroundBinding binding;
+    private MapView mMapView;
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private List<Polyline> polyline1 = new ArrayList<Polyline>();;
+    private Gson responsePosData = new Gson();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.walk_around);
+        binding = WalkAroundBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.walk_around);
 
         // Fine か Coarseのいずれかのパーミッションが得られているかチェックする
         // 本来なら、Android6.0以上かそうでないかで実装を分ける必要がある
@@ -85,7 +120,35 @@ public class WalkAround extends AppCompatActivity implements LocationListener {
         if (location != null) {
             TextView textView = (TextView) findViewById(R.id.textView4);
             textView.setText(String.valueOf( "onCreate() : " + location.getLatitude()) + "," + String.valueOf(location.getLongitude()));
+            currentPosition.setPosition(location.getLatitude(), location.getLongitude());
         }
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+        timer = new Timer();
+        timer.scheduleAtFixedRate(
+            new TimerTask()
+            {
+                @Override
+                public void run()
+                {
+                    addPolyline(currentPosition);
+                }
+            }, 10, 60000);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
     }
 
     protected void findViews(){
@@ -137,7 +200,7 @@ public class WalkAround extends AppCompatActivity implements LocationListener {
         // 利用可能なプロバイダの利用状態が変化したときに呼ばれる
     }
 
-    public void integratedStartAndStop(android.view.View view){
+    public void integratedStartAndStop(View view){
         if(isWalking == true){
             long stoppingTime = System.currentTimeMillis();
             LocalDateTime stoppingLocalTime = LocalDateTime.now();
@@ -154,5 +217,95 @@ public class WalkAround extends AppCompatActivity implements LocationListener {
             ((TextView) findViewById(R.id.button_integrated_startnstop)).setText("散歩時間計測終了");
         }
 
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setScrollGesturesEnabled(true);
+        uiSettings.setZoomGesturesEnabled(true);
+        uiSettings.setTiltGesturesEnabled(true);
+        uiSettings.setRotateGesturesEnabled(true);
+
+        LatLng currentLatLng = new LatLng(this.currentPosition.lat, this.currentPosition.lon);
+        mMap.addMarker(new MarkerOptions().position(currentLatLng).title("Marker in Current Pos"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 16));
+    }
+
+    public void addPolyline(Position pos){
+        // 大雑把な経路情報を作成
+        route.add(pos);
+        deletePolyline();
+        int i = 1;
+        for (Position p : route) {
+            if(i == route.size()) break;
+            LatLng currentLatLng = new LatLng(route.get(i-1).lat, route.get(i-1).lon);
+            LatLng nextLatLng = new LatLng(route.get(i).lat, route.get(i).lon);
+            polyline1.add(mMap.addPolyline(new PolylineOptions()
+                    .clickable(true)
+                    .add(currentLatLng, nextLatLng)));
+            i++;
+        }
+
+        // TODO: 位置情報から取り出した大雑把なルートを位置情報に変換(getRoadRoute())
+        //https://developers.google.com/maps/documentation/roads/snap?hl=ja
+
+    }
+
+    public void deletePolyline(){
+        for(Polyline line : polyline1)
+        {
+            line.remove();
+        }
+    }
+
+    public List<Position> getRouteFromResponse(String resbody){
+        List<Position> posList = new ArrayList<Position>();
+        // TODO: JsonのStringからGSONでクラスに変換する。
+        // TODO: 保管するクラスの実装
+        // https://codechacha.com/ja/java-gson/
+
+        // TODO: クラスからPositionを取り出しリスト化
+        //https://developers.google.com/maps/documentation/roads/snap?hl=ja
+
+        return posList;
+    }
+
+    public String getRoadRoute(Position positionList[] ) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        String url = "https://roads.googleapis.com/v1/snapToRoads?path=";
+        for (Position pos: positionList){
+            url += pos.lat+"."+pos.lon+"|";
+        }
+        url = url.substring(0,url.length()-1);
+        url += "&interpolate=true&key=YOUR_API_KEY";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .method("GET", null)
+                .build();
+        String response = client.newCall(request).execute().body().string();
+
+        return response;
+    }
+
+    public Position getCurrentPosition(){
+        Position pos = new Position();
+
+        return pos;
     }
 }
