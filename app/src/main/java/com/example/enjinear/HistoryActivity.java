40e5db65afc76da9091145750ed8639c09fa6727
shouma0407 <shouma20000407@gmail.com>
2021-08-28 16:59:12 +0900

    :sparkles: mainから履歴への画面遷移完成

---
+++ app/src/main/java/com/example/enjinear/HistoryActivity.java
@@ -0,0 +1,18 @@
+package com.example.enjinear;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+
+
+}
--- app/src/main/java/com/example/enjinear/MainActivity.java
+++ app/src/main/java/com/example/enjinear/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.enjinear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    int data = 0;
+    static public String EXTRA_DATA= "a";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+     public void transitionHistory(android.view.View view){
+        //インデントの作成
+        Intent intent = new Intent(this,HistoryActivity.class);
+        //データをセット
+        //EditText editText = (EditText)this.findViewById(R.id.editText);
+        //intent.putExtra("sendText",editText.getText().toString());
+         intent.putExtra(EXTRA_DATA, data);
+
+         //遷移先の画面を起動
+        startActivity(intent);
+     }
+
+
+
 }
