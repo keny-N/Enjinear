ebd8deac7f3f39161278b1ee3bf6b7dc9b0b9dc7
Schnorlax <michimotorrad@gmail.com>
2021-08-28 23:31:57 +0900

    散歩画面の時間計測ボタンを1個に統一。機能は変わらず

--- app/src/main/java/com/example/enjinear/WalkAround.java
+++ app/src/main/java/com/example/enjinear/WalkAround.java
@@ -1,6 +1,7 @@
 package com.example.enjinear;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,25 +9,29 @@ import java.time.LocalDateTime;
 
 public class WalkAround extends AppCompatActivity {
     long startingTime = 0;
+    boolean isWalking = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.walk_around);
     }
 
-    public void startTimeCount(android.view.View view){
-        startingTime = System.currentTimeMillis();
-        LocalDateTime startingLocalTime = LocalDateTime.now();
-        System.out.println(startingLocalTime);
-    }
-    public void stopTimeCount(android.view.View view){
-        long stoppingTime = System.currentTimeMillis();
-        LocalDateTime stoppingLocalTime = LocalDateTime.now();
-        System.out.println(stoppingLocalTime);
-        long walkingTime = stoppingTime - startingTime;
-        System.out.println(walkingTime);
+    public void integratedStartAndStop(android.view.View view){
+        if(isWalking == true){
+            long stoppingTime = System.currentTimeMillis();
+            LocalDateTime stoppingLocalTime = LocalDateTime.now();
+            System.out.println(stoppingLocalTime);
+            long walkingTime = stoppingTime - startingTime;
+            System.out.println(walkingTime);
+            isWalking = false;
+            ((TextView) findViewById(R.id.button_integrated_startnstop)).setText("散歩時間計測開始");
+        }else{
+            startingTime = System.currentTimeMillis();
+            LocalDateTime startingLocalTime = LocalDateTime.now();
+            System.out.println(startingLocalTime);
+            isWalking = true;
+            ((TextView) findViewById(R.id.button_integrated_startnstop)).setText("散歩時間計測終了");
+        }
 
     }
-
-
 }
