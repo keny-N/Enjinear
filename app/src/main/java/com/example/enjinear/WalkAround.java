8d8148f15d7621f727593e8282a9e7dc06b064d0
Schnorlax <michimotorrad@gmail.com>
2021-08-28 19:38:29 +0900

    散歩画面の遷移ボタンのIDをbutton_walkAroundに変更

--- app/src/main/java/com/example/enjinear/WalkAround.java
+++ app/src/main/java/com/example/enjinear/WalkAround.java
@@ -4,11 +4,28 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.time.LocalDateTime;
+
 public class WalkAround extends AppCompatActivity {
+    long startingTime = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.walk_around);
+    }
+
+    public void startTimeCount(android.view.View view){
+        startingTime = System.currentTimeMillis();
+        LocalDateTime startingLocalTime = LocalDateTime.now();
+        System.out.println(startingLocalTime);
+    }
+    public void stopTimeCount(android.view.View view){
+        long stoppingTime = System.currentTimeMillis();
+        LocalDateTime stoppingLocalTime = LocalDateTime.now();
+        System.out.println(stoppingLocalTime);
+        long walkingTime = stoppingTime - startingTime;
+        System.out.println(walkingTime);
+
     }
 
 
