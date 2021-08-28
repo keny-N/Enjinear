160b13d1b252867056ecf1dd5a18f53b1a44a835
Schnorlax <michimotorrad@gmail.com>
2021-08-28 18:14:00 +0900

    散歩画面への画面遷移ボタンです

--- app/src/main/java/com/example/enjinear/MainActivity.java
+++ app/src/main/java/com/example/enjinear/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.enjinear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void moveToWalkAround(android.view.View view){
+//        Intent intent = new Intent(this,WalkAround.class);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener((View v) -> {
+            startActivity(new Intent(this, WalkAround.class));
+        });
     }
 }
